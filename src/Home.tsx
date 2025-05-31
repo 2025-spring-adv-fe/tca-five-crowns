de7c80e1792e5fd68fa8b0e36fd050161f811477
tsteelematc <tsteele@madisoncollege.edu>
2025-05-31 13:19:59 -0500

    Show/hide copy/paste svgs, i-o-g ? ? ?

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router";
 import { GameResult, GeneralFacts, GoOutsLeaderboardEntry, HighestSingleHandScoreLeaderboardEntry, LeaderboardEntry, validateGameResult } from "./GameResults";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import copyTextToClipboard from 'copy-text-to-clipboard';
 
 export const AppTitle = "Five Crowns Companion";
@@ -29,6 +29,7 @@ export const Home: React.FC<HomeProps> = ({
     , addNewGameResult
 }) => {
 
+    const [showCopyPasteButtons, setShowCopyPasteButtons] = useState(false);
     const copiedModalRef = useRef<HTMLDialogElement | null>(null);
     const pasteModalRef = useRef<HTMLDialogElement | null>(null);
 
@@ -443,16 +444,25 @@ export const Home: React.FC<HomeProps> = ({
                     <div className="flex items-center">
                         <h2
                             className="card-title ml-3 mt-3"
+                            onDoubleClick={
+                                () => setShowCopyPasteButtons(
+                                    !showCopyPasteButtons
+                                )
+                            }
                         >
                             Game History
                         </h2>
-                        <svg
-                            onClick={
-                                () => pasteModalRef.current?.showModal()
-                            }
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" className="size-3 inline ml-auto mr-3">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184" />
-                        </svg>
+                        {
+                            showCopyPasteButtons && (
+                                <svg
+                                    onClick={
+                                        () => pasteModalRef.current?.showModal()
+                                    }
+                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" className="size-3 inline ml-auto mr-3">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184" />
+                                </svg>
+                            )
+                        }
                     </div>
                     {
                         allGames.length > 0
@@ -494,25 +504,29 @@ export const Home: React.FC<HomeProps> = ({
                                                                     className="inline"
                                                                 >
                                                                     {x.players}
-                                                                    <svg
-                                                                        xmlns="http://www.w3.org/2000/svg"
-                                                                        fill="none"
-                                                                        viewBox="0 0 24 24"
-                                                                        strokeWidth="2.5"
-                                                                        stroke="currentColor"
-                                                                        className="size-3 inline ml-1 mb-1"
-                                                                        onClick={
-                                                                            () => {
-                                                                                copyTextToClipboard(
-                                                                                    JSON.stringify(x.result)
-                                                                                );
+                                                                    {
+                                                                        showCopyPasteButtons && (
+                                                                            <svg
+                                                                                xmlns="http://www.w3.org/2000/svg"
+                                                                                fill="none"
+                                                                                viewBox="0 0 24 24"
+                                                                                strokeWidth="2.5"
+                                                                                stroke="currentColor"
+                                                                                className="size-3 inline ml-1 mb-1"
+                                                                                onClick={
+                                                                                    () => {
+                                                                                        copyTextToClipboard(
+                                                                                            JSON.stringify(x.result)
+                                                                                        );
 
-                                                                                copiedModalRef.current?.showModal();
-                                                                            }
-                                                                        }
-                                                                    >
-                                                                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184" />
-                                                                    </svg>
+                                                                                        copiedModalRef.current?.showModal();
+                                                                                    }
+                                                                                }
+                                                                            >
+                                                                                <path strokeLinecap="round" strokeLinejoin="round" d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184" />
+                                                                            </svg>
+                                                                        )
+                                                                    }
                                                                 </div>
                                                             </td>
                                                         </tr>
