797185a3e90668e8cc6fcd272c80156c716dcd21
tsteelematc <tsteele@madisoncollege.edu>
2026-02-09 17:43:48 -0600

    Proper stroke width and save chose lookback

--- src/App.tsx
+++ src/App.tsx
@@ -105,6 +105,35 @@ const App = () => {
   useEffect(
     () => {
 
+      const loadStatRangeLookback = async () => {
+
+        const savedStatRangeLookback = await localforage.getItem<string>("statRangeLookback") ?? "0d";
+
+        if (!ignore) {
+          setStatRangeLookback(savedStatRangeLookback);
+        }
+      };
+
+      //
+      // Build the ignore-sandwich...
+      //
+
+      // Bread on top...
+      let ignore = false;
+
+      loadStatRangeLookback();
+
+      // Bread on bottom...
+      return () => {
+        ignore = true;
+      };
+    }
+    , []
+  );
+  
+  useEffect(
+    () => {
+
       const loadEmail = async () => {
 
         const savedEmail = await localforage.getItem<string>("email") ?? "";
@@ -215,6 +244,16 @@ const App = () => {
     : gameResults.filter(
       x => Date.parse(x.end) >= lookbackDate.getTime()
     );
+
+  const statRangeLookbackSelected = async (
+    event: any,
+    selectedStatRangeLookback: string,
+  ) => {
+    event.currentTarget.blur(); 
+    const savedStatRangeLookback = await localforage.setItem("statRangeLookback", selectedStatRangeLookback);
+    setStatRangeLookback(savedStatRangeLookback);
+  };
+
   //
   // Finally, return the JSX, using any of the state and calculated items
   // from above...
@@ -244,21 +283,22 @@ const App = () => {
                     role="button"
                     className="btn btn-ghost btn-circle p-1"
                   >
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.8} stroke="currentColor" className="size-6">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
                     </svg>
 
                   </div>
                   <ul
                     tabIndex={-1}
-                    className="menu dropdown-content bg-base-200 rounded-box z-1 mt-4 w-52 p-2 shadow-sm">
-                    <li><button className="btn btn-ghost text-lg text-left" onClick={(e) => { e.currentTarget.blur(); setStatRangeLookback("0d");}}>All Time Fun Facts</button></li>
-                    <li><button className="btn btn-ghost text-lg text-left" onClick={(e) => { e.currentTarget.blur(); setStatRangeLookback("7d");}}>Last 7 days</button></li>
-                    <li><button className="btn btn-ghost text-lg text-left" onClick={(e) => { e.currentTarget.blur(); setStatRangeLookback("30d");}}>Last 30 days</button></li>
-                    <li><button className="btn btn-ghost text-lg text-left" onClick={(e) => { e.currentTarget.blur(); setStatRangeLookback("60d");}}>Last 60 days</button></li>
-                    <li><button className="btn btn-ghost text-lg text-left" onClick={(e) => { e.currentTarget.blur(); setStatRangeLookback("90d");}}>Last 90 days</button></li>
-                    <li><button className="btn btn-ghost text-lg text-left" onClick={(e) => { e.currentTarget.blur(); setStatRangeLookback("6mo");}}>Last 6 months</button></li>
-                    <li><button className="btn btn-ghost text-lg text-left" onClick={(e) => { e.currentTarget.blur(); setStatRangeLookback("12mo");}}>Last 12 months</button></li>
+                    className="menu dropdown-content bg-base-200 rounded-box z-1 mt-4 w-52 p-2 shadow-sm"
+                  >
+                    <li><button className="btn btn-ghost text-lg text-left" onClick={(e) => statRangeLookbackSelected(e, "0d")}>All Time Fun Facts</button></li>
+                    <li><button className="btn btn-ghost text-lg text-left" onClick={(e) => statRangeLookbackSelected(e, "7d")}>Last 7 days</button></li>
+                    <li><button className="btn btn-ghost text-lg text-left" onClick={(e) => statRangeLookbackSelected(e, "30d")}>Last 30 days</button></li>
+                    <li><button className="btn btn-ghost text-lg text-left" onClick={(e) => statRangeLookbackSelected(e, "60d")}>Last 60 days</button></li>
+                    <li><button className="btn btn-ghost text-lg text-left" onClick={(e) => statRangeLookbackSelected(e, "90d")}>Last 90 days</button></li>
+                    <li><button className="btn btn-ghost text-lg text-left" onClick={(e) => statRangeLookbackSelected(e, "6mo")}>Last 6 months</button></li>
+                    <li><button className="btn btn-ghost text-lg text-left" onClick={(e) => statRangeLookbackSelected(e, "12mo")}>Last 12 months</button></li>
                   </ul>
                 </div>
                 <button
